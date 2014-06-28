6d153ddff092ddcb58506258a970a39736109d70
nodo_p <gnu-pibe@comunis.com.ar>
2014-06-28 20:04:43 -0300

    Modificaciones en admin.py

--- piqueteca/piqueteca_app/admin.py
+++ piqueteca/piqueteca_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from piqueteca_app.models import *
 
-# Register your models here.
+admin.site.register(Libro)
+admin.site.register(Autora)
+admin.site.register(Lectora)
+admin.site.register(Estado_libro)
+admin.site.register(Estado_prestamo)
+admin.site.register(Editorial)
+admin.site.register(Licencia)
+admin.site.register(Categoria)
+admin.site.register(Tipo)
+admin.site.register(Prestamos)
+
